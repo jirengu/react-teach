f9d89eaf5d9c9bd739be310c1a1c512596479936
sun-shadow <sun-shadow@gmail.com>
2020-04-20 18:25:14 +0800

    数据传递

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-/*
-const Columns = () => (
-  <>
-    <td>小明</td>
-    <td>24</td>
-  </>
-)
-*/
+function First(props) {
+  return <fieldset>
+    <legend>first name </legend>
+    <input value={props.value} onChange={props.onChange}/>
+  </fieldset>
+}
 
+function Last(props) {
+  return <fieldset>
+    <legend>last name </legend>
+    <input value={props.value} onChange={props.onChange}/>
+  </fieldset>
+}
 
-const Columns = () => (
-  <Fragment>
-    <td>小明</td>
-    <td>24</td>
-  </Fragment>
-)
-
-
-const App = (props) => (
-  <table>
-    <tr>
-      <Columns />
-    </tr>
-  </table>
-)
-
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state =  {
+      first: 'af',
+      last: 'b'
+    }    
+  }
+  
+  render() {
+    return (
+      <div>
+        <First value={this.state.first} onChange={(e)=>{this.setState({first: e.target.value})}}/>
+        <Last value={this.state.last} onChange={(e)=>{this.setState({last: e.target.value})}} />
+        <p>full nane is: {this.state.first + ' ' + this.state.last}</p>
+      </div>
+    )
+  }
+}
 
 export default App
