41dfa1e8324f81c7888ce60ecc5a29925230ff6d
sun-shadow <sun-shadow@gmail.com>
2020-04-20 18:53:07 +0800

    refs

--- src/App.js
+++ src/App.js
@@ -1,80 +1,31 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class ErrorBoundary extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { error: null, errorInfo: null }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fileInput = React.createRef();
   }
-  
-  componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
-    this.setState({
-      error: error,
-      errorInfo: errorInfo
-    })
-    console.log(error)
-    console.log(errorInfo)
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(
+      `Selected file - ${this.fileInput.current.files[0].name}`
+    );
   }
-  
-  render() {
-    if (this.state.errorInfo) {
-      // Error path
-      return (
-        <div>
-          <h2>Something went wrong.</h2>
-          <details style={{ whiteSpace: 'pre-wrap' }}>
-            {this.state.error && this.state.error.toString()}
-            <br />
-            {this.state.errorInfo.componentStack}
-          </details>
-        </div>
-      );
-    }
- 
-    return this.props.children
-  }  
-}
 
-class BuggyCounter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  
-  handleClick() {
-    this.setState(({counter}) => ({
-      counter: counter + 1
-    }))
-  }
-  
   render() {
-    if (this.state.counter === 5) {
-      throw new Error('I crashed!')
-    }
-    return <h1 onClick={this.handleClick}>{this.state.counter}</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Upload file:
+          <input type="file" ref={this.fileInput} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
   }
-}
-
-function App() {
-  return (
-    <div>
-      <p>
-        点击计数器
-      </p>
-      <hr />
-      <ErrorBoundary>
-        <p>两个计数器使用同一个Boundary</p>
-        <BuggyCounter />
-        <BuggyCounter />
-      </ErrorBoundary>
-      <hr />
-      <p>两个计数器使用不同的Boundary</p>
-      <ErrorBoundary><BuggyCounter /></ErrorBoundary>
-      <ErrorBoundary><BuggyCounter /></ErrorBoundary>
-    </div>
-  )
 }
 
 
